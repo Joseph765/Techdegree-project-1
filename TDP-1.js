9a169d6378be2af08b5f2520a63d7f93632f919c
Joseph Chauvin <joseph.chauvin102@gmail.com>
2019-06-15 14:16:45 -0500

    Added JavaScript File

---
+++ TDP-1.js
@@ -0,0 +1,24 @@
+var quoteArray = [
+
+  {
+    Quote: 'A successful website does three things It attracts the right kinds of visitors. Guides them to the main services or product you offer. Collect Contact details for future ongoing relation.',
+    Person: 'Mohamed Saad'
+  },
+  {
+    Quote: 'What separates design from art is that design is meant to be... functional.',
+    Person: 'Cameron moll'
+  },
+  {
+    Quote: 'Creating your own portfolio takes time. First you have to choose the technologies among the overwhelming amount of options we have. Am I going to go for React? Angular? PHP? Ruby? What about SEO? Should I try node? What them, where do I host? Once you decided and set everything up, you’ve got to list all your projects manually, add the descriptions, links, images and decide on a design that shows your very best. Suddenly, the simple task of creating a pretty portfolio is overwhelming.',
+    Person: 'Pedro Silva Moreira'
+  },
+  {
+    Quote: 'Getting a quality website is not an expenses but rather an investment.',
+    Person: 'Dr. Christopher Dayagdag'
+  },
+  {
+    Quote: 'Measuring programming progress by lines of code is like measuring aircraft building progress by weight.',
+    Person: 'Bill Gates'
+  }
+
+];
